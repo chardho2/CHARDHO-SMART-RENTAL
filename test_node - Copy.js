5b5c6aa8b116a99c6ff414a669d5362e3040cd34
Muzamil Mohammad K <muzamilmohammadk@gmail.com>
2026-01-14 11:14:57 +0530

    Initial project commit

--- test_node - Copy.js
+++
@@ -1,7 +0,0 @@
-console.log('Test start');
-try {
-    const mongoose = require('mongoose');
-    console.log('Mongoose required');
-} catch (e) {
-    console.error('Mongoose require failed', e);
-}
